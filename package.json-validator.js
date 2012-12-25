6b2eb8e3b65409cbb7c8036f8fb5519a2a33f72d
Nick Sullivan <nick@sullivanflock.com>
2012-12-24 23:38:15 -0700

    warnings about optional fields

--- package.json-validator.js
+++ package.json-validator.js
@@ -18,10 +18,29 @@ function validatePackage(data) {
     out.errors = [];
     out.warnings = [];
 
-    // http://wiki.commonjs.org/wiki/Packages/1.1
+    // http://wiki.commonjs.org/wiki/Packages/1.0
     var map = {
-        "name":     {"type": "string", required: true, format: /^[a-z0-9\.\-_]+$/},
-        "version":  {"type": "string", required: true, format: /^[0-9]+\.[0-9]+\.[0-9+a-zA-Z\.]$/}
+        "name":         {"type": "string", required: true, format: /^[a-z0-9\.\-_]+$/},
+        "description":  {"type": "string", required: true},
+        "version":      {"type": "string", required: true, format: /^[0-9]+\.[0-9]+\.[0-9+a-zA-Z\.]$/},
+        "keywords":     {"type": "array", required: true},
+        "maintainers":  {"type": "array", required: true},
+        "contributors": {"type": "array", required: true},
+        "bugs":         {"type": "object", required: true},
+        "licenses":     {"type": "array", required: true},
+        "repositories": {"type": "object", required: true},
+        "dependencies": {"type": "object", required: true},
+
+        "devDependencies": {"type": "object"},
+        "homepage":     {"type": "string", format: /^http:\/\/[a-z.\-0-9]+/},
+        "os":           {"type": "array"},
+        "cpu":          {"type": "array"},
+        "engine":       {"type": "array"},
+        "builtin":      {"type": "boolean"},
+        "directories":  {"type": "object"},
+        "implements":   {"type": "array"},
+        "scripts":      {"type": "object"},
+        "checksums":    {"type": "object"}
     };
 
     for (var name in map) {
@@ -33,8 +52,15 @@ function validatePackage(data) {
             continue;
         }
 
+        // Optional field check
+        if (!parsed[name] && ! field.required) {
+            out.errors.push("Missing optional field: '" + name + "'");
+            continue;
+        }
+
         // Type checking
-        if (typeof parsed[name] != field.type ) {
+        if ((field.type == "array" && !parsed[name] instanceof Array)
+                || (typeof parsed[name] != field.type) ) {
             out.errors.push("Type for field '" + name + "', was expected to be " + field.type + ", not " + typeof parsed[name]);
             continue;
         }
