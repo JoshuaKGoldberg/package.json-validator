48e2595646e3c86900d84f6c0b95cdb07ee3c6c1
Nick Sullivan <nick@sullivanflock.com>
2012-12-26 12:06:24 -0700

    Refactor js to be cleaner. Framework to support multiple different specs.

--- package.json-validator.js
+++ package.json-validator.js
@@ -1,8 +1,54 @@
 /* Parse the incoming string as JSON, validate it against the spec for package.json
  * See README for more details
  */
-function validatePackage(data) {
+var PJV = {};
+
+PJV.getSpecMap = function(specName) {
+
+    if (specName == "npm") {
+        // https://github.com/isaacs/npm/blob/master/doc/cli/json.md
+        return {};
+    } else if (specName == "commonjs_1.0") {
+        // http://wiki.commonjs.org/wiki/Packages/1.0
+        return {
+            "name":         {"type": "string", required: true, format: /^[a-z0-9\.\-_]+$/},
+            "description":  {"type": "string", required: true},
+            "version":      {"type": "string", required: true, format: /^[0-9]+\.[0-9]+\.[0-9+a-zA-Z\.]$/},
+            "keywords":     {"type": "array", required: true},
+            "maintainers":  {"type": "array", required: true},
+            "contributors": {"type": "array", required: true},
+            "bugs":         {"type": "object", required: true},
+            "licenses":     {"type": "array", required: true},
+            "repositories": {"type": "object", required: true},
+            "dependencies": {"type": "object", required: true},
+
+            "devDependencies": {"type": "object"},
+            "homepage":     {"type": "string", format: /^http:\/\/[a-z.\-0-9]+/},
+            "os":           {"type": "array"},
+            "cpu":          {"type": "array"},
+            "engine":       {"type": "array"},
+            "builtin":      {"type": "boolean"},
+            "directories":  {"type": "object"},
+            "implements":   {"type": "array"},
+            "scripts":      {"type": "object"},
+            "checksums":    {"type": "object"}
+        };
+    } else if (specName == "commonjs_1.1") {
+        // http://wiki.commonjs.org/wiki/Packages/1.1
+        return {};
+    } else {
+        // Unrecognized spec
+        return false;
+    }
+
+};
+
+PJV.validatePackage = function(data, specName, options) {
     var out = {}, parsed;
+    if (!data) {
+        out.critical = {"Empty JSON": "No data to parse"};
+        return out;
+    }
     try {
         parsed = JSON.parse(data);
     } catch (e) {
@@ -15,35 +61,15 @@ function validatePackage(data) {
         return out;
     }
 
+    var map = PJV.getSpecMap(specName);
+    if (specName === false) {
+        out.critical = {"Invalid specification": specName};
+        return out;
+    }
     out.errors = [];
     out.warnings = [];
 
-    // http://wiki.commonjs.org/wiki/Packages/1.0
-    var map = {
-        "name":         {"type": "string", required: true, format: /^[a-z0-9\.\-_]+$/},
-        "description":  {"type": "string", required: true},
-        "version":      {"type": "string", required: true, format: /^[0-9]+\.[0-9]+\.[0-9+a-zA-Z\.]$/},
-        "keywords":     {"type": "array", required: true},
-        "maintainers":  {"type": "array", required: true},
-        "contributors": {"type": "array", required: true},
-        "bugs":         {"type": "object", required: true},
-        "licenses":     {"type": "array", required: true},
-        "repositories": {"type": "object", required: true},
-        "dependencies": {"type": "object", required: true},
-
-        "devDependencies": {"type": "object"},
-        "homepage":     {"type": "string", format: /^http:\/\/[a-z.\-0-9]+/},
-        "os":           {"type": "array"},
-        "cpu":          {"type": "array"},
-        "engine":       {"type": "array"},
-        "builtin":      {"type": "boolean"},
-        "directories":  {"type": "object"},
-        "implements":   {"type": "array"},
-        "scripts":      {"type": "object"},
-        "checksums":    {"type": "object"}
-    };
-
-    for (var name in map) {
+     for (var name in map) {
         var field = map[name];
 
         // Required field check
@@ -54,7 +80,7 @@ function validatePackage(data) {
 
         // Optional field check
         if (!parsed[name] && ! field.required) {
-            out.errors.push("Missing optional field: '" + name + "'");
+            out.warnings.push("Missing optional field: '" + name + "'");
             continue;
         }
 
@@ -77,4 +103,4 @@ function validatePackage(data) {
         return out;
     }
 
-}
+};
