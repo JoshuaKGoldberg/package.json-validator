48e464bd8039e56146adf4488e1b4f6d52157c6d
Nick Sullivan <nick@sullivanflock.com>
2013-03-21 00:20:13 -0700

    Github fetcher working

--- app.js
+++ app.js
@@ -9,13 +9,30 @@ angular.module('pjvFilters', []).filter('validateJson', function() {
 });
 
 // Angular JS controller
-window.PackageCtrl = function($scope) {
+window.PackageCtrl = function($scope, $http) {
     // Default values for the form
     $scope.data = {
         results : "",
         spec : "npm",
         warnings: true,
         recommendations: true
+    };
+
+    $scope.githubFetch = function() {
+
+        var re = /github\.com\/([^\/]+)\/([^\/]+)/,
+            m  = re.exec($scope.data.github);
+            if (!m ) {
+                $scope.data.results = "Invalid github url.";
+                return;
+            }
+
+            var owner = m[1], repo = m[2],
+                apiUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/contents/package.json?callback=JSON_CALLBACK";
+
+        $http.jsonp(apiUrl).success(function(resp) {
+            $scope.data.results = window.Base64.decode(resp.data.content);
+        });
     };
 };
 
