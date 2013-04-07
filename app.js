2cdbbbb6920d800d65e7c7b03211f11c0ea6cbd4
Nick Sullivan <nick@sullivanflock.com>
2013-04-07 10:31:38 -0700

    Send data to google analytics

--- app.js
+++ app.js
@@ -20,13 +20,16 @@ window.PackageCtrl = function($scope, $http) {
 
     $scope.githubFetch = function() {
 
+
         var re = /github\.com\/([^\/]+)\/([^\/]+)/,
             m  = re.exec($scope.data.github);
 
         if (!m ) {
             $scope.data.results = "Invalid github url.";
+            window._gaq.push(['_trackEvent', 'Github', 'Invalid', $scope.data.github]);
             return;
         }
+
 
         var owner = m[1], repo = m[2],
             apiUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/contents/package.json?callback=JSON_CALLBACK";
@@ -34,8 +37,20 @@ window.PackageCtrl = function($scope, $http) {
         $scope.data.results = "Fetching from github...";
 
         $http.jsonp(apiUrl).success(function(resp) {
+            window._gaq.push(['_trackEvent', 'Github', 'Fetch', $scope.data.github]);
             $scope.data.results = window.Base64.decode(resp.data.content);
+        }).error(function(){
+            window._gaq.push(['_trackEvent', 'Github', 'Fail', $scope.data.github]);
         });
+
     };
+
+    var lastResults=null;
+    $scope.$watch('data.results', function() {
+        if ($scope.data.results && lastResults != $scope.data.results) {
+            window._gaq.push(['_trackEvent', 'Validate', 'Text']);
+        }
+        lastResults = $scope.data.results;
+    });
 };
 
