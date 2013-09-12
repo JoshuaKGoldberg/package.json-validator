1d5b49f48611504e9eb5ec8b0376d3661ca8fbf2
Nick Sullivan <nick@sullivanflock.com>
2013-09-12 10:18:42 -0700

    Initial linting infrastructure

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+// http://gruntjs.com/ - Make for javascript
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:package.json>',
+
+        // run jshint on the files, with the options described below
+        jshint: {
+            all: ['*.js', 'test/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        qunit: {
+            files: ["test/qunit-*.html"]
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    grunt.registerTask('test', ['qunit']);
+    grunt.registerTask('citest', ['jshint', 'test']);
+};
